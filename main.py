5683f0521683bd1eb73bd347e8d5f23209f2e880
Muhammad Naufal Rizqullah <opalcool8@gmail.com>
2022-07-18 21:01:18 +0800

    refactor: :fire: a bit pydantic model (schemas)

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name:str
+    price: int
+    discount: int
+
+class ItemOutput(BaseModel):
+    name: str
+    selling_price: int
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"Message": "Welcome"}
+
+
+@app.post("/itmes/", response_model=ItemOutput)
+async def add_item(item: Item):
+    selling_price = item.price - item.discount
+    return {
+        "name": item.name,
+        "selling_price": selling_price
+    }
