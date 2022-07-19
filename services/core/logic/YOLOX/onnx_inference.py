fa89aeca570719c75fa323d375de6d013024da35
Muhammad Naufal Rizqullah <opalcool8@gmail.com>
2022-07-19 15:04:04 +0800

    feat: :sparkles: create costumize onnx_inference to become function ready to use

--- services/core/logic/YOLOX/onnx_inference.py
+++ services/core/logic/YOLOX/onnx_inference.py
@@ -15,62 +15,20 @@ from yolox.data.datasets import COCO_CLASSES
 from yolox.utils import mkdir, multiclass_nms, demo_postprocess, vis
 
 
-def make_parser():
-    parser = argparse.ArgumentParser("onnxruntime inference sample")
-    parser.add_argument(
-        "-m",
-        "--model",
-        type=str,
-        default="yolox.onnx",
-        help="Input your onnx model.",
-    )
-    parser.add_argument(
-        "-i",
-        "--image_path",
-        type=str,
-        default='test_image.png',
-        help="Path to your input image.",
-    )
-    parser.add_argument(
-        "-o",
-        "--output_dir",
-        type=str,
-        default='demo_output',
-        help="Path to your output directory.",
-    )
-    parser.add_argument(
-        "-s",
-        "--score_thr",
-        type=float,
-        default=0.3,
-        help="Score threshould to filter the result.",
-    )
-    parser.add_argument(
-        "--input_shape",
-        type=str,
-        default="640,640",
-        help="Specify an input shape for inference.",
-    )
-    parser.add_argument(
-        "--with_p6",
-        action="store_true",
-        help="Whether your model uses p6 in FPN/PAN.",
-    )
-    return parser
+def fashion_detector(images):
+    
+    PATH_ONNX_MODEL = "../model/fashion_best_yolox_s.onnx/"
+    SCORE_THRESHOLD = 0.1
 
-
-if __name__ == '__main__':
-    args = make_parser().parse_args()
-
-    input_shape = tuple(map(int, args.input_shape.split(',')))
-    origin_img = cv2.imread(args.image_path)
+    input_shape = (640, 640)
+    origin_img = images
     img, ratio = preprocess(origin_img, input_shape)
 
-    session = onnxruntime.InferenceSession(args.model)
+    session = onnxruntime.InferenceSession(PATH_ONNX_MODEL)
 
     ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
     output = session.run(None, ort_inputs)
-    predictions = demo_postprocess(output[0], input_shape, p6=args.with_p6)[0]
+    predictions = demo_postprocess(output[0], input_shape, p6=False)[0]
 
     boxes = predictions[:, :4]
     scores = predictions[:, 4:5] * predictions[:, 5:]
@@ -81,12 +39,10 @@ if __name__ == '__main__':
     boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2]/2.
     boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3]/2.
     boxes_xyxy /= ratio
-    dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
+    dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=SCORE_THRESHOLD)
     if dets is not None:
         final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
         origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
-                         conf=args.score_thr, class_names=COCO_CLASSES)
+                         conf=SCORE_THRESHOLD, class_names=COCO_CLASSES)
 
-    mkdir(args.output_dir)
-    output_path = os.path.join(args.output_dir, os.path.basename(args.image_path))
-    cv2.imwrite(output_path, origin_img)
+    cv2.imwrite("output/output.jpg", origin_img)
