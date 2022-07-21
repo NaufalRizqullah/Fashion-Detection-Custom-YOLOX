4c8014cc24a888521f6e91ab2451eb412a940e66
Muhammad Naufal Rizqullah <opalcool8@gmail.com>
2022-07-21 15:21:50 +0800

    feat: :fire: add requirement fix, and add locust for testing loading

---
+++ locust.py
@@ -0,0 +1,32 @@
+
+
+from locust import HttpUser, SequentialTaskSet, task
+
+
+class YoloxTask(SequentialTaskSet):
+
+    @task
+    def detection(self):
+        with open("test_image.jpeg", "rb") as image:
+            self.client.post(
+                "/v1/detect",
+                files = {"im": image}
+            )
+
+class YoloxTaskV2(SequentialTaskSet):
+
+    @task
+    def detection(self):
+        with open("test_image.jpeg", "rb") as image:
+            self.client.post(
+                "/v1/detectv2",
+                files = {"im": image}
+            )
+
+class LoadTester(HttpUser):
+    # define a host/web ti hit
+    host = "https://simple-fashion-detection.herokuapp.com"
+
+    # identify task according to class we created before, this mean we can pass more than one
+    tasks = [YoloxTask, YoloxTaskV2]
+
